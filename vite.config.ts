ba074c0975f1ea29bf475e28468db81d91ab85b7
Octavianu <stanescu.matei.octavian@gmail.com>
2026-04-10 22:14:21 +0300

    incercare fix auth numar 3

--- vite.config.ts
+++ vite.config.ts
@@ -30,6 +30,7 @@ export default defineConfig({
         ],
       },
       workbox: {
+        navigateFallbackDenylist: [/^\/api\//],
         runtimeCaching: [
           {
             urlPattern: /^\/api\/.*/i,
